831916a43b1ad6a56fa74107e322674880e555ad
Teo Goerens <goerens_teo@hotmail.com>
2023-05-01 22:49:45 -0300

    agregado de array con algunos productos en JS

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    name: "Dom Perignon",
+    price: 100000,
+    category: "Champagnes",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur!",
+    image: "imgs/domperignon.jpg",
+    stock: 500,
+  },
+  {
+    name: "Baron B",
+    price: 5000,
+    category: "Sparkling Wines",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur!",
+    image: "imgs/baronb.jpg",
+    stock: 10000,
+  },
+  {
+    name: "Chandon",
+    price: 2500,
+    category: "Sparkling Wines",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur!",
+    image: "imgs/chandon.jpg",
+    stock: 50000,
+  },
+  {
+    name: "Latitud 33",
+    price: 1500,
+    category: "Still Wines",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur!",
+    image: "imgs/latitud.jpg",
+    stock: 25000,
+  },
+  {
+    name: "Terrazas Reserva",
+    price: 2500,
+    category: "Still Wines",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur!",
+    image: "imgs/terrazasreserva.jpg",
+    stock: 15000,
+  },
+  {
+    name: "Hennessy",
+    price: 12000,
+    category: "Spirits",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur!",
+    image: "imgs/hennessy.jpg",
+    stock: 50,
+  },
+];
