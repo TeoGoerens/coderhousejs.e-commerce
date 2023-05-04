b1989dd5a5966827475cbfecb3e2f172c372e4b1
Teo Goerens <goerens_teo@hotmail.com>
2023-05-03 21:22:37 -0300

    funciones de agregado al carrito por cantidades corregida

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* -------------------- USEFUL ARRAYS -------------------- */
+
 const products = [
   {
     id: 1,
@@ -78,10 +80,23 @@ const categories = [
   { id: "spirits", name: "Spirits" },
 ];
 
+/* -------------------- USEFUL VARIABLES -------------------- */
+
 /* I need to define a variable containing the empty div in the HTML whose id is #products */
 const productsContainer = document.querySelector("#products");
 const buttonCategories = document.querySelectorAll(".button__categories");
 const cartContainer = document.querySelector("#cart__container");
+let addToCartButtons = document.querySelectorAll(".products__card__button");
+let cartProducts = [];
+
+/* -------------------- USEFUL FUNCTIONS -------------------- */
+
+function updateCartButtons() {
+  addToCartButtons = document.querySelectorAll(".products__card__button");
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", pushToCart);
+  });
+}
 
 function displayProducts(filteredProducts) {
   productsContainer.innerHTML = "";
@@ -101,9 +116,31 @@ function displayProducts(filteredProducts) {
     /* Each newly created div with the corresponding information & style should now be included in the HTML in the products' container whose id is #products */
     productsContainer.append(productsCard);
   });
+  updateCartButtons();
 }
 
+function displayCart(cartProducts) {
+  cartContainer.innerHTML = "";
+  cartProducts.forEach((product) => {
+    const cartProductsCard = document.createElement("div");
+    /* For each newly created div, I will assign the corresponding class to make sure that the CSS properties are assigned */
+    cartProductsCard.classList.add("cartProducts__card");
+    /* Within each newly created div, I will add all the necessary information to be displayed in the card */
+    cartProductsCard.innerHTML = `
+                <h3 class="products__card__title">${product.name}</h3>
+                <p class="products__card__price">Price: ${product.price}</p>
+                <p class="products__card__quantity">${product.quantity}</p>`;
+
+    /* Each newly created div with the corresponding information & style should now be included in the HTML in the cart's container */
+    cartContainer.append(cartProductsCard);
+  });
+}
+
+/* I call the displayProducts() function for the first time to make sure that the products are all loaded when the page is refreshed */
+
 displayProducts(products);
+
+/* -------------------- USEFUL EVENT LISTENERS -------------------- */
 
 buttonCategories.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -121,14 +158,6 @@ buttonCategories.forEach((button) => {
   });
 });
 
-/* Now that the whole products are displayed in the HTML, I save in a new variable all the ADD TO CART buttons to make sure I will be able to add an event listener to them */
-const addToCartButtons = document.querySelectorAll(".products__card__button");
-let cartProducts = [];
-
-addToCartButtons.forEach((button) => {
-  button.addEventListener("click", pushToCart);
-});
-
 function pushToCart(e) {
   const idButton = e.currentTarget.id;
   const addToCartProduct = products.find(
@@ -136,25 +165,19 @@ function pushToCart(e) {
   );
 
   if (addToCartProduct != undefined && addToCartProduct.stock > 0) {
-    cartProducts.push(addToCartProduct);
+    if (
+      cartProducts.some((product) => product.name === addToCartProduct.name)
+    ) {
+      const repeatedProductIndex = cartProducts.findIndex(
+        (product) => product.name === addToCartProduct.name
+      );
+      cartProducts[repeatedProductIndex].quantity++;
+    } else {
+      addToCartProduct.quantity = 1;
+      cartProducts.push(addToCartProduct);
+    }
   }
-
-  displayCart();
-}
-
-function displayCart() {
-  const lastCartProducts = cartProducts.slice(-1)[0];
-  const cartProductsCard = document.createElement("div");
-  /* For each newly created div, I will assign the corresponding class to make sure that the CSS properties are assigned */
-  cartProductsCard.classList.add("cartProducts__card");
-  /* Within each newly created div, I will add all the necessary information to be displayed in the card */
-  cartProductsCard.innerHTML = `
-            <h3 class="products__card__title">${lastCartProducts.name}</h3>
-            <p class="products__card__price">Price: ${lastCartProducts.price}</p>
-            <p class="products__card__description">${lastCartProducts.description}</p>`;
-
-  /* Each newly created div with the corresponding information & style should now be included in the HTML in the cart's container */
-  cartContainer.append(cartProductsCard);
+  displayCart(cartProducts);
 }
 
 /* Empty Cart event listener */
