ca5bac3629564942c92dafc8606e82321a1ba431
Teo Goerens <goerens_teo@hotmail.com>
2023-05-28 13:38:50 -0300

    agregado de toastify para cuando un producto es eliminado del carrito

--- js/cart.js
+++ js/cart.js
@@ -29,7 +29,7 @@ function displayCart() {
     cartProductsCard.innerHTML = `
     <img src="${product.image}" class="cartProducts__card__image" alt="${product.name}"/>
     <h3 class="cartProducts__card__title">${product.name}</h3>
-    <p class="cartProducts__card__price">$${cartProductPriceFormatted}</p>
+    <p class="cartProducts__card__price">Price: $${cartProductPriceFormatted}</p>
     <div class="cartProducts__quantity__container">
         <button class="cartProducts__quantity__reducebutton" id="${product.id}">-</button>
         <p class="cartProducts__card__quantity">${product.quantity}</p>
@@ -79,6 +79,26 @@ function reduceItem(event) {
     (product) => product.id == event.currentTarget.id
   );
   if (cartProducts[cartProductIndex].quantity === 1) {
+    /* Notification after product has been removed from cart */
+    Toastify({
+      text: "Product removed from cart",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      style: {
+        background:
+          "linear-gradient(to right, rgb(246, 43, 43), rgb(243, 161, 8))",
+        fontWeight: "200",
+        borderRadius: "5rem",
+        fontSize: "0.75rem",
+      },
+      offset: {
+        x: "2rem", // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+        y: "9rem", // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
     cartProducts.splice(cartProductIndex, 1);
   } else {
     cartProducts[cartProductIndex].quantity--;
