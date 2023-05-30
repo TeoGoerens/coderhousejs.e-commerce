c12425f010000f76703ee4528affaa80ee31a5c6
Teo Goerens <goerens_teo@hotmail.com>
2023-05-30 13:17:55 -0300

    arreglo del mensaje desplegado cuando el carrito esta vacio

--- js/cart.js
+++ js/cart.js
@@ -13,51 +13,63 @@ let increaseItemButton;
 const emptyCartButton = document.querySelector("#button__emptyCart");
 let cartTotalAmount = document.querySelector("#cart__total");
 
+const emptyCartInnerText = `
+  <h4 class="emptyCart__message">
+    Your cart is currently empty. Go check <a href="/index.html">our products</a>
+  </h4>
+`;
+
 function displayCart() {
   cartContainer.innerHTML = "";
-  cartProducts.forEach((product) => {
+
+  if (cartProducts.length === 0) {
     const cartProductsCard = document.createElement("div");
-    /* For each newly created div, I will assign the corresponding class to make sure that the CSS properties are assigned */
-    cartProductsCard.classList.add("cartProducts__card");
-    /* Within each newly created div, I will add all the necessary information to be displayed in the card */
-    let totalPerProduct = new Intl.NumberFormat("de-DE").format(
-      product.price * product.quantity
-    );
-    let cartProductPriceFormatted = new Intl.NumberFormat("de-DE").format(
-      product.price
-    );
-    cartProductsCard.innerHTML = `
-    <img src="${product.image}" class="cartProducts__card__image" alt="${product.name}"/>
-    <h3 class="cartProducts__card__title">${product.name}</h3>
-    <p class="cartProducts__card__price">Price: $${cartProductPriceFormatted}</p>
-    <div class="cartProducts__quantity__container">
-        <button class="cartProducts__quantity__reducebutton" id="${product.id}">-</button>
-        <p class="cartProducts__card__quantity">${product.quantity}</p>
-        <button class="cartProducts__quantity__increasebutton" id="${product.id}">+</button>
-    </div>
-    <p class="cartProducts__card__total">Total: $${totalPerProduct}</p>`;
-
-    /* Each newly created div with the corresponding information & style should now be included in the HTML in the cart's container */
+    cartProductsCard.innerHTML = emptyCartInnerText;
     cartContainer.append(cartProductsCard);
-
     calculateTotalCartAmount(cartProducts);
+  } else {
+    cartProducts.forEach((product) => {
+      const cartProductsCard = document.createElement("div");
+      /* For each newly created div, I will assign the corresponding class to make sure that the CSS properties are assigned */
+      cartProductsCard.classList.add("cartProducts__card");
+      /* Within each newly created div, I will add all the necessary information to be displayed in the card */
+      let totalPerProduct = new Intl.NumberFormat("de-DE").format(
+        product.price * product.quantity
+      );
+      let cartProductPriceFormatted = new Intl.NumberFormat("de-DE").format(
+        product.price
+      );
+      cartProductsCard.innerHTML = `
+      <img src="${product.image}" class="cartProducts__card__image" alt="${product.name}"/>
+      <h3 class="cartProducts__card__title">${product.name}</h3>
+      <p class="cartProducts__card__price">Price: $${cartProductPriceFormatted}</p>
+      <div class="cartProducts__quantity__container">
+          <button class="cartProducts__quantity__reducebutton" id="${product.id}">-</button>
+          <p class="cartProducts__card__quantity">${product.quantity}</p>
+          <button class="cartProducts__quantity__increasebutton" id="${product.id}">+</button>
+      </div>
+      <p class="cartProducts__card__total">Total: $${totalPerProduct}</p>`;
 
-    /* Update increase & reduce items' buttons */
-    reduceItemButton = document.querySelectorAll(
-      ".cartProducts__quantity__reducebutton"
-    );
-    increaseItemButton = document.querySelectorAll(
-      ".cartProducts__quantity__increasebutton"
-    );
+      /* Each newly created div with the corresponding information & style should now be included in the HTML in the cart's container */
+      cartContainer.append(cartProductsCard);
+      calculateTotalCartAmount(cartProducts);
 
-    increaseItemButton.forEach((button) => {
-      button.addEventListener("click", increaseItem);
+      /* Update increase & reduce items' buttons */
+      reduceItemButton = document.querySelectorAll(
+        ".cartProducts__quantity__reducebutton"
+      );
+      increaseItemButton = document.querySelectorAll(
+        ".cartProducts__quantity__increasebutton"
+      );
+
+      increaseItemButton.forEach((button) => {
+        button.addEventListener("click", increaseItem);
+      });
+      reduceItemButton.forEach((button) => {
+        button.addEventListener("click", reduceItem);
+      });
     });
-
-    reduceItemButton.forEach((button) => {
-      button.addEventListener("click", reduceItem);
-    });
-  });
+  }
 }
 
 displayCart();
@@ -129,6 +141,10 @@ emptyCartButton.addEventListener("click", () => {
       localStorage.removeItem("cartProducts");
       cartContainer.innerHTML = "";
       cartProducts = [];
+      const cartProductsCard = document.createElement("div");
+      cartProductsCard.innerHTML = emptyCartInnerText;
+      cartContainer.append(cartProductsCard);
+
       calculateTotalCartAmount(cartProducts);
 
       Swal.fire({
